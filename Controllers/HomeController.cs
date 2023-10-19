f64d93e6a7819e67215a0c9662ddb5b1594edcf3
seemzythebillionare <waseemdarkwa@gmail.com>
2023-10-19 16:55:14 +0000

    contact and about us page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,6 +34,12 @@ namespace GenAIAssignment.Controllers
             return View();
         }
 
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
