71438032ca0967d373c5257547302a7a2ad3ac9e
seemzythebillionare <waseemdarkwa@gmail.com>
2023-10-19 14:46:01 +0000

    ai jobs page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,12 @@ namespace GenAIAssignment.Controllers
             return View();
         }
 
+
+        public IActionResult Jobs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
