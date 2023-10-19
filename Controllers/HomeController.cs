d6228f889c0bb1c677499c0cd48abf3977eee16a
wassem7 <waseemdarkwa@gmail.com>
2023-10-19 14:07:52 +0000

    readmore pages and footer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace GenAIAssignment.Controllers
             return View();
         }
 
+        public IActionResult GenAI()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
